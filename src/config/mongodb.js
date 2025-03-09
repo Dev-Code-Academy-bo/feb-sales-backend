dc72919c847bc213b78db83138dd44ed94924805
Fernando Hurtado <rockfercho@gmail.com>
2025-03-08 22:58:13 -0400

    implemented mongo atlas

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const DB = 'backendfeb2025';
 const REF = 'mongodb://localhost:27017/';
 
+const DB_ATLAS = 'mongodb+srv://rockfercho:<PasswordDB>@feb2025nodejs.h9wb9.mongodb.net/?retryWrites=true&w=majority&appName=feb2025nodejs';
+
 function connect() {
   mongoose.connect( REF+DB, {
+  //mongoose.connect( DB_ATLAS, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   });
